4f9641d0b906e4368c1f25dcc7e28de0789f436d
Alla-Goptsii <goptsy.a@gmail.com>
2022-11-04 22:39:16 +0200

    add structure project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,6 @@
+export const App = () => {
+  return <div>Yuuu-huuu</div>;
+};
+
+// https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
+// user_id: 29926103;
