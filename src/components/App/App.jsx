b0d58479e6adfafe183a94ef78ed8c06ed08c82b
Alla-Goptsii <goptsy.a@gmail.com>
2022-11-10 09:49:15 +0200

    correct app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,36 +19,32 @@ export default class App extends Component {
   };
 
   fetchImg = () => {
-    setTimeout(() => {
-      return fetch(
-        `${this.state.URL}?q=${this.state.imageName}&page=${this.state.page}&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-      )
-        .then(response => {
-          if (response.ok) return response.json();
+    return fetch(
+      `${this.state.URL}?q=${this.state.imageName}&page=${this.state.page}&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then(response => {
+        if (response.ok) return response.json();
 
-          return Promise.reject(
-            new Error(`Nothing was found for your request`)
-          );
-        })
-        .then(gallery => {
-          if (!gallery.total) {
-            toast.error('Nothing was found for your request');
+        return Promise.reject(new Error(`Nothing was found for your request`));
+      })
+      .then(gallery => {
+        if (!gallery.total) {
+          toast.error('Nothing was found for your request');
+        }
+        const selectedProperties = gallery.hits.map(
+          ({ id, largeImageURL, webformatURL, tags }) => {
+            return { id, largeImageURL, webformatURL, tags };
           }
-          const selectedProperties = gallery.hits.map(
-            ({ id, largeImageURL, webformatURL, tags }) => {
-              return { id, largeImageURL, webformatURL, tags };
-            }
-          );
-          this.setState(prevState => {
-            return {
-              gallery: [...prevState.gallery, ...selectedProperties],
-              status: 'resolved',
-              totalHits: gallery.total,
-            };
-          });
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }, 1000);
+        );
+        this.setState(prevState => {
+          return {
+            gallery: [...prevState.gallery, ...selectedProperties],
+            status: 'resolved',
+            totalHits: gallery.total,
+          };
+        });
+      })
+      .catch(error => this.setState({ error, status: 'rejected' }));
   };
 
   componentDidUpdate(prevProps, pervState) {
