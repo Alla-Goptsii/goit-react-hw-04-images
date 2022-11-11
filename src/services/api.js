4d2596a6fed82fb0f131f000997e31b6a2b72dfd
Alla-Goptsii <goptsy.a@gmail.com>
2022-11-11 15:56:20 +0200

    add api.

--- src/services/api.js
+++ src/services/api.js
@@ -1,30 +1,51 @@
-// export function imagesFetch(imageName) {
-//   const URL = 'https://pixabay.com/api/';
-//   const API_KEY = '29905727-ba938f57a9499389ab5e34ef4';
+import axios from 'axios';
 
-//   const queryParams = `?q=${nextName}&page=1&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
-//   const url = this.state.URL + queryParams;
+const API_KEY = '29905727-ba938f57a9499389ab5e34ef4';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-//   if (prevName !== nextName) {
-//     this.setState({ status: 'pending', gallery: [], page: 1 });
+export const fetchImg = async (imageName, page, setStatus) => {
+  try {
+    const response = await axios.get(
+      `?q=${imageName}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    );
+    if (response.data.hits.length < 1) {
+      console.log(response.data);
+      setStatus('rejected');
+    }
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+// .then(response => {
+//   return response.data;
+// });
 
-//     return fetch(url).then(response => {
+// fetchImg = () => {
+//   return fetch(
+//     `${this.state.URL}?q=${this.state.imageName}&page=${this.state.page}&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+//   )
+//     .then(response => {
 //       if (response.ok) return response.json();
 
-//       return Promise.reject(
-//         new Error(`Nothing was found for your request ${imageName}`)
+//       return Promise.reject(new Error(`Nothing was found for your request`));
+//     })
+//     .then(gallery => {
+//       if (!gallery.total) {
+//         toast.error('Nothing was found for your request');
+//       }
+//       const selectedProperties = gallery.hits.map(
+//         ({ id, largeImageURL, webformatURL, tags }) => {
+//           return { id, largeImageURL, webformatURL, tags };
+//         }
 //       );
-//     });
-//   }
-// }
-
-// const baseURL = 'https://pixabay.com/api';
-// const API_KEY = '29905727-ba938f57a9499389ab5e34ef4';
-
-// export function imagesFetch(imageName, page) {
-//   return fetch(
-//     `https://pixabay.com/api?q=${imageName}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-//   ).then(response => {
-//     return response.data;
-//   });
-// }
+//       this.setState(prevState => {
+//         return {
+//           gallery: [...prevState.gallery, ...selectedProperties],
+//           status: 'resolved',
+//           totalHits: gallery.total,
+//         };
+//       });
+//     })
+//     .catch(error => this.setState({ error, status: 'rejected' }));
+// };
