a53ad79089a4f251e81a497bdb5922c4d1c0e66a
Alla-Goptsii <99249674+Alla-Goptsii@users.noreply.github.com>
2022-11-11 16:18:46 +0200

    updates fetch from api

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
-import Searchbar from '../Searchbar/Searchbar';
 import { ToastContainer, toast } from 'react-toastify';
+
+import { fetchImg } from '../../services/api';
+import Searchbar from '../Searchbar/Searchbar';
 import ImageGallery from '../ImageGallery/ImageGallery';
 import { Button } from '../Button/Button';
 import { Loader } from '../Loader/Loader';
@@ -9,8 +11,6 @@ import { Container } from './App.styled';
 export default class App extends Component {
   state = {
     imageName: '',
-    URL: 'https://pixabay.com/api/',
-    API_KEY: '29905727-ba938f57a9499389ab5e34ef4',
     gallery: [],
     error: '',
     status: 'idle',
@@ -18,46 +18,33 @@ export default class App extends Component {
     totalHits: null,
   };
 
-  fetchImg = () => {
-    return fetch(
-      `${this.state.URL}?q=${this.state.imageName}&page=${this.state.page}&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then(response => {
-        if (response.ok) return response.json();
+  componentDidUpdate(_, pervState) {
+    const prevName = pervState.imageName;
+    const nextName = this.state.imageName;
+    const { imageName, page, status } = this.state;
 
-        return Promise.reject(new Error(`Nothing was found for your request`));
-      })
-      .then(gallery => {
-        if (!gallery.total) {
+    if ((prevName !== nextName || pervState.page) !== this.state.page) {
+      this.setState({ status: 'pending' });
+      fetchImg(imageName, page, status).then(data => {
+        if (data.hits.lenght === 0) {
           toast.error('Nothing was found for your request');
+        } else {
+          this.setState({ status: 'resolved' });
         }
-        const selectedProperties = gallery.hits.map(
+        const selectedProperties = data.hits.map(
           ({ id, largeImageURL, webformatURL, tags }) => {
             return { id, largeImageURL, webformatURL, tags };
           }
         );
+
         this.setState(prevState => {
           return {
             gallery: [...prevState.gallery, ...selectedProperties],
             status: 'resolved',
-            totalHits: gallery.total,
+            totalHits: this.state.gallery.total,
           };
         });
-      })
-      .catch(error => this.setState({ error, status: 'rejected' }));
-  };
-
-  componentDidUpdate(prevProps, pervState) {
-    const prevName = pervState.imageName;
-    const nextName = this.state.imageName;
-
-    if (prevName !== nextName) {
-      this.setState({ status: 'pending', gallery: [], page: 1 });
-      this.fetchImg();
-    }
-    if (pervState.page !== this.state.page) {
-      this.setState({ status: 'pending' });
-      this.fetchImg();
+      });
     }
   }
 
@@ -70,7 +57,7 @@ export default class App extends Component {
   };
 
   render() {
-    const { gallery, totalHits, status, error } = this.state;
+    const { gallery, status, error } = this.state;
 
     if (status === 'pending') {
       return <Loader />;
@@ -82,7 +69,7 @@ export default class App extends Component {
         {error && <h1>{error.message}</h1>}
         <ToastContainer autoClose={2000} />
         {gallery.length > 0 && <ImageGallery gallery={gallery} />}
-        {totalHits > gallery.length && <Button onClick={this.loadMore} />}
+        {this.state.status === 'resolved' && <Button onClick={this.loadMore} />}
       </Container>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -3,49 +3,17 @@ import axios from 'axios';
 const API_KEY = '29905727-ba938f57a9499389ab5e34ef4';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export const fetchImg = async (imageName, page, setStatus) => {
+export const fetchImg = async (imageName, page, status) => {
   try {
     const response = await axios.get(
       `?q=${imageName}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
     );
     if (response.data.hits.length < 1) {
       console.log(response.data);
-      setStatus('rejected');
+      status('rejected');
     }
     return response.data;
   } catch (error) {
     console.log(error.message);
   }
 };
-// .then(response => {
-//   return response.data;
-// });
-
-// fetchImg = () => {
-//   return fetch(
-//     `${this.state.URL}?q=${this.state.imageName}&page=${this.state.page}&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-//   )
-//     .then(response => {
-//       if (response.ok) return response.json();
-
-//       return Promise.reject(new Error(`Nothing was found for your request`));
-//     })
-//     .then(gallery => {
-//       if (!gallery.total) {
-//         toast.error('Nothing was found for your request');
-//       }
-//       const selectedProperties = gallery.hits.map(
-//         ({ id, largeImageURL, webformatURL, tags }) => {
-//           return { id, largeImageURL, webformatURL, tags };
-//         }
-//       );
-//       this.setState(prevState => {
-//         return {
-//           gallery: [...prevState.gallery, ...selectedProperties],
-//           status: 'resolved',
-//           totalHits: gallery.total,
-//         };
-//       });
-//     })
-//     .catch(error => this.setState({ error, status: 'rejected' }));
-// };
